c1daf85855c074ca147142fbcea8230b179c9721
Matthias Schmitz <maschmit@ce03458.speedport_w_724v_typ_a_05011603_05_028>
2018-04-20 15:23:13 +0200

    Created project

---
+++ capexp4m/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace capexp4m.Core
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new capexp4mPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ capexp4m/capexp4mPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace capexp4m.Core
+{
+    public partial class capexp4mPage : ContentPage
+    {
+        public capexp4mPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
